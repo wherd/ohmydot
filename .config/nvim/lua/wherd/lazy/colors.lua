3533e8f695526f52e1f8672ee890b0db92d4d243
Sérgio Leal <me@wherd.dev>
2024-06-13 09:44:26 +0100

    setup neovim

---
+++ .config/nvim/lua/wherd/lazy/colors.lua
@@ -0,0 +1,15 @@
+function ColorMyPencils(color)
+	color = color or "catppuccin"
+	vim.cmd.colorscheme(color)
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme "catppuccin-mocha"
+	end
+}
